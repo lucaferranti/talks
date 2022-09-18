94dd9cdd9e305aba2b9db1230e147b3b7034052e
Luca Ferranti <49938764+lucaferranti@users.noreply.github.com>
2022-09-18 16:03:54 +0300

    add juh-intro

---
+++ juh-intro.jl
@@ -0,0 +1,73 @@
+### A Pluto.jl notebook ###
+# v0.19.11
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 252794b0-34e3-11ed-0369-598233f524b2
+html"""
+<h1>Julia Users Helsinki - 2022-09-15</h1>
+<center>
+<img style="width: 100%;" src="https://raw.githubusercontent.com/julia-users-helsinki/julia-users-helsinki.github.io/main/page/_assets/juh-logo.svg"/>
+</center>
+"""
+
+# ╔═╡ d2168709-4331-40ab-8ca6-0c52ce984060
+md"""
+## Purpose of the meetups
+
+- bring together everyone interested in Julia
+- networking, sharing, collaboration and most of all fun!
+- hopefully meetings will be regularly (once a month?)
+- talks to share your work / interests with the community are encouraged
+- check out the webpage <https://julia-users-helsinki.github.io> (still under construction, your input welcome!)
+"""
+
+# ╔═╡ 5cd9d24b-80cd-4fb2-b9f9-c6ec23de688d
+md"""
+## Agenda for today
+
+- **16:00**: Welcoming and introduction
+- **16:15**: Fabricio Oliveira, *Teaching and research using Julia in the Gamma-opt research group*
+- **16:45 -- 17:30**: free hanging out (coffee/tea available)
+- **17:30**: Dinner at Fat Lizard
+"""
+
+# ╔═╡ e86a31f4-60e4-4dfd-ad20-450eccca53e3
+md"""
+## Introduction
+- who you are, what you do, what you use Julia for, what attracted you to Julia, etc.
+
+## Julia Overview
+
+- Modern fast, dynamic general-purpose programming language
+- Particularly attractive for researcher in data science, scientific computing, etc.
+- Solves the two languages problem: *Reads like Python, runs like C*
+- Thriving ecosystem for several applications, especially scientific computing
+- Active community in slack, zulip, discord, discourse, forem, youtube, twitter, github. Check out <https://julialang.org/community/>
+- If you haven't Julia already, get it now at <https://julialang.org/downloads>
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.0"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─252794b0-34e3-11ed-0369-598233f524b2
+# ╟─d2168709-4331-40ab-8ca6-0c52ce984060
+# ╟─5cd9d24b-80cd-4fb2-b9f9-c6ec23de688d
+# ╟─e86a31f4-60e4-4dfd-ad20-450eccca53e3
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
